9591bbf77cffbbb4bf22841d5319f694b8add303
Slawomir Borowiec <przodownik@tlen.pl>
2016-09-10 09:59:31 +0200

    hawtio added

--- src/main/java/pl/java/scalatech/EipBootApplication.java
+++ src/main/java/pl/java/scalatech/EipBootApplication.java
@@ -10,14 +10,18 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.hawt.springboot.EnableHawtio;
+import io.hawt.web.AuthenticationFilter;
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
 @Slf4j
+@EnableHawtio
 public class EipBootApplication implements CommandLineRunner{
     
     
 	public static void main(String[] args) throws IOException {	  
+	    System.setProperty(AuthenticationFilter.HAWTIO_AUTHENTICATION_ENABLED, "false");
 		SpringApplication.run(EipBootApplication.class, args);
 		
 	}
